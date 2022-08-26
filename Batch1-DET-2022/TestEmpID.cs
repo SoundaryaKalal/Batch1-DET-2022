c1a9fed23515caa4eddd08034ade7b505f901954
Soundarya <soundaryakalal31@gmai.com>
2022-08-26 05:58:58 -0700

    Changes

---
+++ Batch1-DET-2022/TestEmpID.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestEmpID
+    {
+        public static void Main()
+        {
+            EmpID e = new EmpID(22855, "monika", 584123, "banglore", 914866767, 30000, 1000);
+            Console.WriteLine($"emp netsal is{e.GetNetSal()}");
+            Console.WriteLine($"emp grade is {e.GetEmpGrade()}");
+        }
+    }
+}
