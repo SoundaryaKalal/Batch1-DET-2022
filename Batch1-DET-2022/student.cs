8c1e19adae3212bd019c24e832fdfd7d8af79b97
Soundarya <soundaryakalal31@gmai.com>
2022-08-29 20:41:15 -0700

    student sience and commerce

---
+++ Batch1-DET-2022/student.cs
@@ -0,0 +1,16 @@
+﻿namespace BATCH1_DET_2022
+{
+    public abstract class Student
+    {
+        public int regno { get; set; }
+        public string Name { get; set; }
+
+
+
+        public abstract double GetAvgMarks();
+
+
+
+    }
+}
+
---
+++ Batch1-DET-2022/student1.cs
@@ -0,0 +1,15 @@
+﻿namespace BATCH1_DET_2022
+{
+    internal class ScienceStudent : Student
+    {
+        public int physics { get; set; }
+        public int chemistry { get; set; }
+
+        public int maths { get; set; }
+
+        public override double GetAvgMarks()
+        {
+            return (physics + chemistry + maths) / 3;
+        }
+    }
+}
