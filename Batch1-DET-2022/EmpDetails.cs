e21dc7f1eb6eb581230bb912dac0f465800107ee
Soundarya <soundaryakalal31@gmai.com>
2022-09-22 15:38:23 -0700

    Added student

---
+++ Batch1-DET-2022/EmpDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Batch1_DET_2022
+{
+    internal class EmpDetails
+    {
+        internal int GetNetSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Batch1-DET-2022/InvalidBirthYearException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+
+
+    [Serializable]
+    internal class InvalidBirthYearException : Exception
+    {
+        public InvalidBirthYearException()
+        {
+        }
+
+        public InvalidBirthYearException(string? message) : base(message)
+        {
+        }
+
+        public InvalidBirthYearException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidBirthYearException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
+  
